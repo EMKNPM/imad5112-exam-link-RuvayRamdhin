23ba47e634313a9df27aebf7a198cbe6dd6ddfef
RuvayRamdhin <ST10522098@myemeris.edu.za>
2026-06-10 09:56:13 +0200

    Doing the declarations and typecasting

--- IMAD exam/app/src/main/java/com/example/imadexam/MainActivity.kt
+++ IMAD exam/app/src/main/java/com/example/imadexam/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.imadexam
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //Declarations
+     private lateinit var btnStart: Button
+    private lateinit var btnExit: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //typecasting
+        btnStart = findViewById(R.id.btnStart)
+        btnExit = findViewById(R.id.btnExit)
+
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
