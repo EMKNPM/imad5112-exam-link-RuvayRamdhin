013fd554370b197b0b8df305d97390e18dd5c86f
RuvayRamdhin <ST10522098@myemeris.edu.za>
2026-06-10 11:37:56 +0200

    Done with the typecasting for the main screen backend

--- IMAD exam/app/src/main/java/com/example/imadexam/MainScreen.kt
+++ IMAD exam/app/src/main/java/com/example/imadexam/MainScreen.kt
@@ -19,21 +19,31 @@ class MainScreen : AppCompatActivity() {
     private lateinit var tvDescription2: TextView
     private lateinit var tvDescription3: TextView
     private lateinit var btnDetailScreen: Button
-    private lateinit var btnStart: Button
-
-
-
-
-
-
-
-
-
+    private lateinit var btnAddGear: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_screen)
+
+        //Typecasting
+        edtShelterItems = findViewById(R.id.edtShelterItems)
+        edtFoodItems = findViewById(R.id.edtFoodItems)
+        edtSafetyItems = findViewById(R.id.edtSafetyItems)
+        tvDescription1 = findViewById(R.id.tvDescription1)
+        tvDescription2 = findViewById(R.id.tvDescription2)
+        tvDescription3 = findViewById(R.id.tvDescription3)
+        btnDetailScreen = findViewById(R.id.btnDetailScreen)
+        btnAddGear = findViewById(R.id.btnAddGear)
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
