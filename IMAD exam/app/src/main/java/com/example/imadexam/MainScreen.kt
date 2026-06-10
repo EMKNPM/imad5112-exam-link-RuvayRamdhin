b36d59aa731f10d50121bf1828412e912929229f
RuvayRamdhin <ST10522098@myemeris.edu.za>
2026-06-10 12:36:03 +0200

    Changing the declarations and typecasting

--- IMAD exam/app/src/main/java/com/example/imadexam/MainScreen.kt
+++ IMAD exam/app/src/main/java/com/example/imadexam/MainScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -14,14 +15,16 @@ import androidx.core.view.WindowInsetsCompat
 class MainScreen : AppCompatActivity() {
 
     //Declarations
-    private lateinit var edtShelterItems: EditText
-    private lateinit var edtFoodItems: EditText
-    private lateinit var edtSafetyItems: EditText
-    private lateinit var tvDescription1: TextView
-    private lateinit var tvDescription2: TextView
-    private lateinit var tvDescription3: TextView
+    private lateinit var edtItem: EditText
+    private lateinit var tvDescription: TextView
     private lateinit var btnDetailScreen: Button
     private lateinit var btnAddGear: Button
+    private lateinit var btnSaveDescription: Button
+    private lateinit var spnCategory: Spinner
+
+    //making an array to save all the info
+    val campingStuff = arrayOf("Shelter","Food", "Safety")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,16 +32,14 @@ class MainScreen : AppCompatActivity() {
         setContentView(R.layout.activity_main_screen)
 
         //Typecasting
-        edtShelterItems = findViewById(R.id.edtShelterItems)
-        edtFoodItems = findViewById(R.id.edtFoodItems)
-        edtSafetyItems = findViewById(R.id.edtSafetyItems)
-        tvDescription1 = findViewById(R.id.tvDescription1)
-        tvDescription2 = findViewById(R.id.tvDescription2)
-        tvDescription3 = findViewById(R.id.tvDescription3)
-        btnDetailScreen = findViewById(R.id.btnDetailScreen)
+        edtItem = findViewById(R.id.edtItem)
+        tvDescription = findViewById(R.id.tvDescription)
+        btnSaveDescription = findViewById(R.id.btnSaveDescription)
         btnAddGear = findViewById(R.id.btnAddGear)
+        spnCategory = findViewById(R.id.spnCategory)
 
 
+        
         //Making the button for the next screen
         btnDetailScreen.setOnClickListener {
             intent = Intent(this, DetailedViewScreen::class.java)
@@ -60,30 +61,6 @@ class MainScreen : AppCompatActivity() {
     //Making a function to save the all the info
     //that the user has entered
     private fun saveGearData(){
-
-        //Makes sure that only thing that can be entered is a string
-        val shelterItem = edtShelterItems.toString()
-        val foodItem = edtFoodItems.toString()
-        val safetyItem = edtSafetyItems.toString()
-
-        //Another error handling to make sure all inputs fields are completed
-        if (shelterItem.isEmpty() || foodItem.isEmpty() || safetyItem.isEmpty()){
-            Toast.makeText(this, "Please complete all the fields!", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        val description1 = tvDescription1.text.toString()
-        val description2 = tvDescription2.text.toString()
-        val description3 = tvDescription3.text.toString()
-
-        if (description1.isEmpty() || description2.isEmpty() || description3.isEmpty()){
-            Toast.makeText(this, "Please complete all the fields!", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-
-
-
 
 
 
