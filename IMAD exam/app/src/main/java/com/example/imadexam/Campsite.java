38f7cee8c6ae34ac9d44a0a97860306a9976f704
RuvayRamdhin <ST10522098@myemeris.edu.za>
2026-06-10 11:28:36 +0200

    Adding the declarations of the declarations

--- IMAD exam/app/src/main/java/com/example/imadexam/Campsite.java
+++ IMAD exam/app/src/main/java/com/example/imadexam/Campsite.java
@@ -2,12 +2,4 @@ package com.example.imadexam;
 
 public class Campsite {
 
-    //Storing the names of the items
-
-
-
-
-
-
-
 }
--- IMAD exam/app/src/main/java/com/example/imadexam/MainScreen.kt
+++ IMAD exam/app/src/main/java/com/example/imadexam/MainScreen.kt
@@ -1,12 +1,35 @@
 package com.example.imadexam
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainScreen : AppCompatActivity() {
+
+    //Declarations
+    private lateinit var edtShelterItems: EditText
+    private lateinit var edtFoodItems: EditText
+    private lateinit var edtSafetyItems: EditText
+    private lateinit var tvDescription1: TextView
+    private lateinit var tvDescription2: TextView
+    private lateinit var tvDescription3: TextView
+    private lateinit var btnDetailScreen: Button
+    private lateinit var btnStart: Button
+
+
+
+
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
